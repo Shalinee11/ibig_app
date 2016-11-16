67a8e280db955b34f2ab696294f975cd5a5b89a9
shalinee11 <shalinee11052012@gmail.com>
2016-11-16 03:47:11 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Tokio Marine iBig App"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
+  test "should get log in" do
+    get static_pages_login_url
+    assert_response :success
+    assert_select "title", "Log in | #{@base_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
 end
